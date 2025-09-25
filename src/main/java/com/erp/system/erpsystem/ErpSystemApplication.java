d8c3c4b0ef86c7a188cdd065018dd7c5cd97c71a
javi2754 <jjaaiiggaanneesshh77@gmail.com>
2025-09-25 20:27:50 +0530

    Initial commit

---
+++ src/main/java/com/erp/system/erpsystem/ErpSystemApplication.java
@@ -0,0 +1,13 @@
+package com.erp.system.erpsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erp/system/erpsystem/ErPsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erp.system.erpsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErPsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
